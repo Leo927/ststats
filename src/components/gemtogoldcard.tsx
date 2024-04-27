821f825880503ce5bc74fa9b3e409ff8bb10c4e6
Songhao Li <songhao.li@hotmail.com>
2024-04-27 16:17:41 -0400

    renamed items

--- src/components/gemtogoldcard.tsx
+++ src/components/gemtogoldcard.tsx
@@ -3,7 +3,7 @@ export default function GemToGoldCard(props: any, index: number) {
     return (
         <div key={index}>
             <div style={{ display: 'flex' }}>
-                {props.item.tier} {props.item.tag1} {props.item.uid}
+                <Image src="/assets/MiscIcons/icon_global_level_item_s_r.png" alt="Level Icon" width={30} height={30} /> {props.item.tier} {props.item.tag1} {props.item.uid}
                 offer <Image src="/assets/Currencies/icon_global_gem.png" alt="Gold Icon" width={25} height={25} /> {props.item.gemsprice}
 
                 request <Image src="/assets/Currencies/icon_global_gold.png" alt="Gold Icon" width={25} height={25} /> {props.item.goldprice}
