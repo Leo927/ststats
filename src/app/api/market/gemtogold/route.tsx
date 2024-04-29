0e3cca53cda18ccd435aba27bff4c65a4e10c1bf
Songhao Li <songhao.li@hotmail.com>
2024-04-28 19:46:56 -0600

    optimized gemtogold

--- src/app/api/market/gemtogold/route.tsx
+++ src/app/api/market/gemtogold/route.tsx
@@ -16,23 +16,7 @@ export async function GET(request: NextRequest) {
          * Then group by tier and fine the highest goldPrice over gemsPrice ratio and the uid and tag1 
          * 
          * */
-        const query = sql`SELECT * FROM 
-        (SELECT DISTINCT ON (m.uid, m.tag1) 
-            m.tier, 
-            m.uid, 
-            m.tag1, 
-            m.gemsPrice, 
-            r.goldPrice, 
-            (r.goldPrice / m.gemsPrice)  AS ratio,
-            rank() over (partition by m.tier order by r.goldPrice / m.gemsPrice desc) rn,
-            REPLACE(t.value, ' Dabbler', '') AS type
-        FROM Market m
-        LEFT OUTER JOIN Market r ON m.uid = r.uid AND m.tag1 = r.tag1 AND r.tType = 'r' AND r.goldPrice > 0
-        LEFT OUTER JOIN iteminfo i ON m.uid = i.uid
-        LEFT OUTER JOIN translation t ON CONCAT('ascension_upgrade_', i.type, '_00') = t.key
-        WHERE m.tType = 'o' AND m.gemsPrice > 0 AND r.goldPrice > 0
-        ) WHERE rn = 1 ORDER BY tier               
-        `;
+        const query = sql`SELECT * FROM gemtogold;`;
         const data = await query;
 
 
--- src/app/api/market/update/route.tsx
+++ src/app/api/market/update/route.tsx
@@ -61,6 +61,24 @@ VALUES ${data.map((i: any) => formatRow(i))};
 `.replaceAll('\n', '');
         sql.query("DELETE FROM Market;");
         const result = sql.query(query);
+        sql.query(`DROP TABLE gemtogold;
+            CREATE TABLE gemtogold (SELECT * FROM 
+                (SELECT DISTINCT ON (m.uid, m.tag1) 
+                    m.tier, 
+                    m.uid, 
+                    m.tag1, 
+                    m.gemsPrice, 
+                    r.goldPrice, 
+                    (r.goldPrice / m.gemsPrice)  AS ratio,
+                    rank() over (partition by m.tier order by r.goldPrice / m.gemsPrice desc) rn,
+                    REPLACE(t.value, ' Dabbler', '') AS type
+                FROM Market m
+                LEFT OUTER JOIN Market r ON m.uid = r.uid AND m.tag1 = r.tag1 AND r.tType = 'r' AND r.goldPrice > 0
+                LEFT OUTER JOIN iteminfo i ON m.uid = i.uid
+                LEFT OUTER JOIN translation t ON CONCAT('ascension_upgrade_', i.type, '_00') = t.key
+                WHERE m.tType = 'o' AND m.gemsPrice > 0 AND r.goldPrice > 0
+                ) WHERE rn = 1 ORDER BY tier);
+        )`)
         return NextResponse.json({ result }, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error }, { status: 500 });
