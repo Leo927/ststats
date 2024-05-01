8bf171ab53928f00eb9696883231f05e4b0eeee3
Songhao Li <songhao.li@hotmail.com>
2024-05-01 09:02:14 -0600

    added dynamic to gtg

--- src/app/api/market/gemtogold/route.tsx
+++ src/app/api/market/gemtogold/route.tsx
@@ -4,6 +4,8 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse, NextRequest } from 'next/server';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
 
 export async function GET(request: NextRequest) {
     try {
